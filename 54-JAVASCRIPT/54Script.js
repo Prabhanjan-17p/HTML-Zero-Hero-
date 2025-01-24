87e34c121525a999cfb0341055c126cb9122ce05
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-01-24 13:19:48 +0530

    Starting Javascript

---
+++ 54-JAVASCRIPT/54Script.js
@@ -0,0 +1,21 @@
+alert("hello world");
+
+// this cmd is used to show the task on the console section 
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
+
+var a = prompt("Enter your number")
+console.log("YOUR NUMBER IS "+a)
+
+var isTrue = confirm("you sure to leave your system")
+if(isTrue){
+    console.log("good bye")
+}
+else{
+    console.log("thank you")
+}
+
+document.title = "hey i'am good"
+
+// document.body.style.backgroundColor="red"
