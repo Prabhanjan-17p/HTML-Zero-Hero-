cff9676fce994a354e8bdfdabf15cf5da2583727
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2025-01-26 20:30:33 +0530

    JAvaScript Conditional If Else ElseIFLader

---
+++ 56-JAvaScriptConditionalIfElseElseIFLader/56JAvaScriptConditionalIfElseElseIFLader.js
@@ -0,0 +1,106 @@
+console.log("this the conditional operater video")
+
+let age = 23
+let grace = 3
+
+// if(age > 18){
+//     console.log("vote for time")
+
+// }else{
+//     console.log("sorry")
+// }
+
+//1-areticmetic operator
+
+//addition op
+// console.log(age + grace)
+
+//subtraction op
+// console.log(age - grace)
+
+//multiplication
+// console.log(age * grace)
+
+//division op
+// console.log(age / grace)
+
+//Exponentiation op(it show the power only )
+// console.log(age ** grace)
+
+//modulus op(it show the reminder part only)
+// console.log(age % grace)
+
+//increment op
+
+
+//decrement op
+
+
+//2 -Assigniment op
+//  age += 5
+//  age += age
+//  age -= age
+//  age *= age
+//  age /= age
+//  age %= age
+//  age **= age
+ //age = age +age
+//  console.log(age)
+
+// 3-Comparission op 
+if(age == 18 ){
+    console.log("you can drive")
+}
+if(age != 18 ){
+    console.log("you can drive")
+}
+if(age === 18 ){
+    console.log("you can drive")
+}
+if(age !== 18 ){
+    console.log("you can drive")
+}
+if(age > 18 ){
+    console.log("you can drive")
+}
+if(age < 18 ){
+    console.log("you can drive")
+}
+if(age <= 18 ){
+    console.log("you can drive")
+}
+if(age >= 18 ){
+    console.log("you can drive")
+}
+
+let a =6
+let b = 8
+let c = a>b ? a-b : b-a
+console.log(c)
+// if(a > b ){
+//     console.log(a-b)
+// }
+// else{
+//     console.log(b-a)
+// }
+
+// 4-Logical op 
+// && , || ,!
+
+// javaScript comment line 
+// -1 // (single line)
+// - 2 /* */ (multiline)
+
+// ifElse lader
+if(age != 18){
+    console.log("drive u can")
+}
+else if(age == 0){
+    console.log("bhakk teri mai ka ....")
+}
+else if(age >= 18){
+    console.log("thank you for watching")
+}
+else{
+    console.log("push ur limit first")
+}
